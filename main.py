0056e10d55a3d83449401dd7047ae69e15813692
xendexxx <thegamincrow@gmail.com>
2021-12-06 11:25:49 -0500

    basic web scraping foundation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("aaa")
+from bs4 import BeautifulSoup
+import requests
 
-input("Hi")
+source = requests.get('https://www.basketball-reference.com/leagues/NBA_2021_per_game.html').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+article = soup.find('tbody')
+
+player = soup.find('div', class_='full_table')
+
+print(player)
