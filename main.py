fd8917024f84674c822bd8512f156dde7244dd26
xendexxx <thegamincrow@gmail.com>
2021-12-08 23:35:17 -0500

    changed idea
    instead of going through all players and their stats, instead the program should go to the individual's bref page and search "2021 gamestats".
    
    So far it can find all the stats, now I just need to get the input from the user to find a specific stat, problem is I can't use findAll because bs4

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.basketball-reference.com/leagues/NBA_2021_per_game.html').text
+source = requests.get('https://www.basketball-reference.com/players/a/achiupr01.html').text
 
 soup = BeautifulSoup(source, 'lxml')
-tbody = soup.find("div", {"id": "all_per_game_stats"})
-fullt = tbody.find('tbody')
+tbody = soup.find('tbody')
+pergame = tbody.find(class_="full_table")
+classrite = pergame.find(class_="right")
 
-# data = fullt.find("player")
-# use soup.get to get obscure attributes other than class_=
-data = fullt.findAll('tr', class_="full_table")
 
-#print(tbody)
-print(data)
+
+data = input("what data")
+
+find = classrite.get("datastat=" + data
+                     )
+print(find
+      )
+
+
