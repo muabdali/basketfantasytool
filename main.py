9747dfda1bdc0a66486f61fc1a62038780683c98
xendexxx <thegamincrow@gmail.com>
2021-12-14 00:53:02 -0500

    User can now input first and last name and it'll return the player's all time stats.
    Used slice to grab 5 letters of the last name + 2 letters of the first name to fit the format bref uses.
    
    next steps should be to organize stat tables by year.

--- main.py
+++ main.py
@@ -2,22 +2,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.basketball-reference.com/players/a/achiupr01.html').text
+first = ()
+first_slice = ()
+last = ()
 
-soup = BeautifulSoup(source, 'lxml')
-tbody = soup.find('tbody')
-pergame = tbody.find(class_="full_table")
-classrite = pergame.find(class_="right")
-
-
-
-data = input("what data")
-
-find = classrite.get("datastat=" + data
-                     )
-first=()
-
-last=()
 
 def askname():
     global first
@@ -25,5 +13,39 @@ def askname():
     global last
     last = input(str("Last Name of Player?"))
     print("Confirmed, loading up " + first + " " + last)
+# asks user for player name
 
+askname()
+
+
+first_slice_result = (first[:2])
+last_slice_result = (last[:5])
+print(first_slice_result)
+print(last_slice_result)
+# slices player's name so it can match the format bref uses
+first_slice_resultA = str(first_slice_result)
+last_slice_resultA = str(last_slice_result)
+
+first_last_slice = last_slice_resultA + first_slice_resultA
+
+lower = first_last_slice.lower() + "01"
+
+start_letter = (last[:1])
+lower_letter = (start_letter.lower())
+# grabs the letter bref uses for organization
+
+print(lower)
+source = requests.get('https://www.basketball-reference.com/players/' + lower_letter + '/' + lower + '.html').text
+
+soup = BeautifulSoup(source, 'lxml')
+tbody = soup.find('tbody')
+pergame = tbody.find(class_="full_table")
+classrite = pergame.find(class_="right")
+
+print(pergame)
+
+data = input("what data")
+
+find = classrite.get("datastat=" + data
+                     )
 
