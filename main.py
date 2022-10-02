f6b8ecd37bd630302c7c90d3e0e0cc5aca05f4f1
Muslum <muslumabdali12345@gmail.com>
2022-10-02 18:45:54 -0400

    setup framework for next update

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
+# pip install bs4
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-#pip install fuzzywuzzy
+# pip install fuzzywuzzy
 from fuzzywuzzy import process
 
-#pip install choice
+# pip install choice
 import choice
 
 
-
+# asking for player's name
 def askname():
     playerNameInput = input(str("Enter the player's name -> "))
     return playerNameInput
 
 
 # Get all player IDs
-player_df = pd.read_csv('https://www.basketball-reference.com/short/inc/sup_players_search_list.csv', header=None)
-player_df = player_df.rename(columns={0:'id',
-                                      1:'playerName',
-                                      2:'years'})
+player_df = pd.read_csv(
+    'https://www.basketball-reference.com/short/inc/sup_players_search_list.csv', header=None)
+player_df = player_df.rename(columns={0: 'id',
+                                      1: 'playerName',
+                                      2: 'years'})
 playersList = list(player_df['playerName'])
 
 # asks user for player name
@@ -27,18 +29,25 @@ playerNameInput = askname()
 
 
 # Find closest matches
-search_match = pd.DataFrame(process.extract(f'{playerNameInput}', playersList))
-search_match = search_match.rename(columns={0:'playerName',1:'matchScore'})
 
-matches = pd.merge(search_match, player_df, how='inner', on='playerName').drop_duplicates().reset_index(drop=True)
-choices = [': '.join(x) for x in list(zip(matches['playerName'], matches['years']))]
+# process.extra(f'{WHAT TO LOOK FOR}', WHERE TO LOOK) Checks playersList (19-25) for the player name given
+# matchscore uses fuzzywuzzy to find the closest to given name
+
+search_match = pd.DataFrame(process.extract(f'{playerNameInput}', playersList))
+search_match = search_match.rename(columns={0: 'playerName', 1: 'matchScore'})
+
+matches = pd.merge(search_match, player_df, how='inner',
+                   on='playerName').drop_duplicates().reset_index(drop=True)
+choices = [': '.join(x) for x in list(
+    zip(matches['playerName'], matches['years']))]
 
 # Choice the match
 playerChoice = choice.Menu(choices).ask()
 playerName, years = playerChoice.split(': ')
 
 # Get that match players id
-match = player_df[(player_df['playerName'] == playerName) & (player_df['years'] == years)]
+match = player_df[(player_df['playerName'] == playerName)
+                  & (player_df['years'] == years)]
 
 baseUrl = 'https://www.basketball-reference.com/players'
 playerId = match.iloc[0]['id']
@@ -51,9 +60,36 @@ soup = BeautifulSoup(html, 'html.parser')
 statList = ['fga_per_mp', 'fg3_per_mp', 'ft_per_mp', 'random']
 for stat in statList:
     try:
-        statTd = soup.find('td', {'data-stat':stat})
+        statTd = soup.find('td', {'data-stat': stat})
         print(statTd['data-stat'], statTd.text)
     except:
         print(f'{stat} stat not found')
 
-# test
+# tests
+
+# NEW WORK STARTS HERE
+
+
+# new work, search for stats only in new year
+playerNameInput = askname()
+
+
+# find closest options to given name
+search_match = pd.DataFrame(process.extract(f'{playerNameInput}', playersList))
+search_match = search_match.rename(columns={0: 'playerName', 1: 'matchScore'})
+
+matches = pd.merge(search_match, player_df, how='inner',
+                   on='playerName').drop_duplicates().reset_index(drop=True)
+choices = [': '.join(x) for x in list(
+    zip(matches['playerName'], matches['years']))]
+
+playerChoice = choice.Menu(choices).ask()
+playerName, years = playerChoice.split(': ')
+
+match = player_df[(player_df['playerName'] == playerName)
+                  & (player_df['years'] == years)]
+
+baseUrl = 'https://www.basketball-reference.com/players'
+playerId = match.iloc[0]['id']
+
+url = f'{baseUrl}/{playerId[0]}/{playerId}.html'
