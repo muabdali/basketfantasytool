4a12bb933d48d03e23a259dec54c7d4f88c06628
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-10-14 19:34:48 -0400

    need to make a loop that makes the playergrabber go through each teamID and grab it's players and append them to a db

--- rosterimport.py
+++ rosterimport.py
@@ -3,18 +3,34 @@
 
 import requests
 leagueID = 454981630
-url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{leagueID}?forTeamId=10&scoringPeriodId=1&view=mRoster'
-r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
+teamID = 1
+
 
 
 # gets players from json and puts them in a dictionary
-def rosterGet():
+def rosterGet(givenLeagueID, givenTeamID):
+    url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?forTeamId={givenTeamID}&scoringPeriodId=1&view=mRoster'
+    r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
     for e in r.json()['teams'][0]['roster']['entries']:
         #print(e['playerPoolEntry']['player']['fullName'])
         rosterPlayer_dict.append(e['playerPoolEntry']['player']['fullName'])
-
+        print(e['playerId'])
 # dictionary with all the players from linked team
 rosterPlayer_dict=[]
 
-rosterGet()
+rosterGet(leagueID, teamID)
 print(rosterPlayer_dict)
+
+
+"""
+TEAM ID CORRESPONDS WITH ORDER ON https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/454981630?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav
+
+ROSTER INFORMATION ON EACH INDIVIDUAL TEAM ID IS AT https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/454981630?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav
+
+e.g 4th team under members[] has TeamId = 4.
+
+
+
+
+
+"""
