e51d0cfb80856e5b321a1de5cb22b34befea896b
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-10-18 11:43:05 -0400

    created dictionary for teamID, next step is to combine both into coloumns/rows in a dataframe and have the players be the data

--- rosterimport.py
+++ rosterimport.py
@@ -12,22 +12,27 @@ def teamNget(givenLeagueID):
     for e in r.json()['members']:
         lastName = (e['lastName'])
         lastName_Print = f'Team {lastName}'
+        rosterTeam_dict.append(e['lastName'])
 
 # gets players from json and puts them in a dictionary
 def rosterGet(givenLeagueID, givenTeamID):
     url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?forTeamId={givenTeamID}&scoringPeriodId=1&view=mRoster'
     r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
     for e in r.json()['teams'][0]['roster']['entries']:
-        print(type(url))
         #print(e['playerPoolEntry']['player']['fullName'])
         rosterPlayer_dict.append(e['playerPoolEntry']['player']['fullName'])
-        print(e['playerId'])
 # dictionary with all the players from linked team
 rosterPlayer_dict=[]
-
+rosterTeam_dict=[]
 teamNget(leagueID)
-#rosterGet(leagueID, teamID)
-print(rosterPlayer_dict)
+rosterGet(leagueID, teamID)
+print(rosterTeam_dict)
+
+
+
+
+
+
 
 
 """
@@ -36,9 +41,5 @@ TEAM ID CORRESPONDS WITH ORDER ON https://fantasy.espn.com/apis/v3/games/fba/sea
 ROSTER INFORMATION ON EACH INDIVIDUAL TEAM ID IS AT https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/454981630?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav
 
 e.g 4th team under members[] has TeamId = 4.
-
-
-
-
 
 """
