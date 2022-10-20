cd3cea48a6c98e0304172d9ed3ccfcf6532b4c34
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-10-19 21:26:55 -0400

    Program can now print the team name, then the team's players. next step is seperate them in a dataframe

--- rosterimport.py
+++ rosterimport.py
@@ -1,34 +1,53 @@
 # pull player from espn team
 
+from fileinput import filename
+import json
+from discord import Team
+
+
+
+path = './'
+fileName = 'members'
 
 import requests
 leagueID = 454981630
-teamID = 1
+#teamID = 1
 
 
-def teamNget(givenLeagueID):
-    url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav'
-    r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
-    for e in r.json()['members']:
-        lastName = (e['lastName'])
-        lastName_Print = f'Team {lastName}'
-
-# gets players from json and puts them in a dictionary
-def rosterGet(givenLeagueID, givenTeamID):
-    url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?forTeamId={givenTeamID}&scoringPeriodId=1&view=mRoster'
-    r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
-    for e in r.json()['teams'][0]['roster']['entries']:
-        print(type(url))
-        #print(e['playerPoolEntry']['player']['fullName'])
-        rosterPlayer_dict.append(e['playerPoolEntry']['player']['fullName'])
-        print(e['playerId'])
-# dictionary with all the players from linked team
 rosterPlayer_dict=[]
 
-teamNget(leagueID)
-#rosterGet(leagueID, teamID)
-print(rosterPlayer_dict)
 
+def PlayerGrab(givenLeagueID, teamID):
+    teamID = teamID
+    mrl = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?forTeamId={teamID}&scoringPeriodId=1&view=mRoster'
+    p = requests.get(mrl, headers = {"User-Agent": "Mozilla/5.0"})
+    for t in p.json()['teams'][0]['roster']['entries']:
+            #print(e['playerPoolEntry']['player']['fullName'])
+            rosterPlayer_dict.append(t['playerPoolEntry']['player']['fullName'])
+            playerNameinLoop = (t['playerPoolEntry']['player']['fullName'])
+            print(playerNameinLoop)
+
+
+def TeamRoster(givenLeagueID):
+    teamID = 1
+    while teamID < 10:
+        url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav'
+        r = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
+        for e in r.json()['members']:
+            lastName = (e['lastName'])
+            lastName_Print = f'Team {lastName}'
+            print(lastName_Print)
+            PlayerGrab(givenLeagueID, teamID)
+            teamID = teamID + 1
+
+        
+
+            
+                    
+
+
+TeamRoster(leagueID)
+#print(rosterPlayer_dict)
 
 """
 TEAM ID CORRESPONDS WITH ORDER ON https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/454981630?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav
