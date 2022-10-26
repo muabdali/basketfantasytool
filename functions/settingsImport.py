948f060f8858f57d499e013bada86eec53d434c4
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-10-26 19:38:41 -0400

    created new py file for importing values for keys (points = 1, steal = 4 etc).

---
+++ functions/settingsImport.py
@@ -0,0 +1,43 @@
+# ESPN Fantasy league settings import
+# accesses scoringItems and returns values. All the point values are hidden under a dataID system. See statID.md for more info.
+
+from fileinput import filename
+import json
+from discord import Team
+import numpy as np
+import requests
+
+
+path = './'
+fileName = 'members'
+leagueID = 454981630
+
+# includes dictionary for all key value pairs (points, steals etc)
+# for legend on keys (0 = points, 6 = rounds etc) check statID.md
+def importSettings(givenLeagueID):
+    global statsDict
+    url = f'https://fantasy.espn.com/apis/v3/games/fba/seasons/2023/segments/0/leagues/{givenLeagueID}?view=mSettings&view=mRoster&view=mTeam&view=modular&view=mNav'
+    p = requests.get(url, headers = {"User-Agent": "Mozilla/5.0"})
+    statsDict = {
+        '0' :[],
+        '1' :[],
+        '6' :[],
+        '11' :[],
+        '13' :[],
+        '14' :[],
+        '15' :[],
+        '16' :[],
+        '17' :[],
+        '2' :[],
+        '3' :[]
+            }
+    for t in p.json()['settings']['scoringSettings']['scoringItems']:
+        statID = (t['statId'])
+        pointValue = (t['points'])
+        statsID = str(statID)
+        statsDict[statsID].append(pointValue)
+
+
+
+importSettings(leagueID)
+print(statsDict)
