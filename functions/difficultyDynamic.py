97a31a451ac91f33425e272283cf4ff99bc76d3a
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-12-15 19:59:52 -0500

    Started making difficultyLists dynamic for teams by referencing power rankings

---
+++ functions/difficultyDynamic.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Make a request to the website
+url = "https://www.cbssports.com/nba/powerrankings/"
+page = requests.get(url)
+
+# Parse the HTML of the page
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Find all the elements with the class "teamname"
+teamname_elements = soup.find_all(class_="team-name")
+
+
+# Print the text of each element
+for element in teamname_elements:
+    teamName_text = element.get_text()
+
+    print(teamName_text)
