75685b04334f778147021e647a347c09d2a07f66
Muslum Abd Ali <muslumabdali1234@gmail.com>
2022-12-18 01:57:07 -0500

    created list to store team names, next step is to divide the list into difficulty rankings

--- functions/difficultyDynamic.py
+++ functions/difficultyDynamic.py
@@ -10,10 +10,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 # Find all the elements with the class "teamname"
 teamname_elements = soup.find_all(class_="team-name")
+teamNamesList = []
 
 
 # Print the text of each element
 for element in teamname_elements:
     teamName_text = element.get_text()
-
+    teamNamesList.append(teamName_text)
     print(teamName_text)
+
+
